95cac9791e89938c284d26c79cc21b69b40ac17f
Chikinayev <bagitbek1@mail.ru>
2023-05-28 22:12:06 +0600

    fix(): prifile page 2

--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {LoginServices} from "../../services/login.services";
+import {tap} from "rxjs";
 
 @Component({
   selector: 'app-menu',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent {
+  userName: string;
+
+  constructor(private readonly loginServices: LoginServices) {
+    this.init();
+  }
+
+  init() {
+    this.loginServices.user$.pipe(
+      tap(value => {
+        if (value) {
+          this.userName = value.fio;
+        }
+      })
+    ).
+      subscribe();
+  }
 
 }
