e3125e3c391f1e240bd5ec826d17b104993c0a01
Bagitbek <bagitbek1@gmail.com>
2023-05-28 15:16:41 +0600

    new components were added

---
+++ src/app/pages/organizator-profile/organizator-profile.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-organizator-profile',
+  templateUrl: './organizator-profile.component.html',
+  styleUrls: ['./organizator-profile.component.scss']
+})
+export class OrganizatorProfileComponent {
+
+}
