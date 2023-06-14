2a3a67ae7ce3eae47c921fcf45b488d5a41a5b93
Chikinayev <bagitbek1@mail.ru>
2023-06-14 09:44:00 +0600

    fix

--- src/app/pages/event/event.component.ts
+++ src/app/pages/event/event.component.ts
@@ -19,6 +19,7 @@ export class EventComponent implements OnDestroy{
   eventPhotos: File;
   private readonly subs = new SubSink();
   currentUser: UserDto;
+  errorMessage: string;
 
 
   constructor(private readonly route: ActivatedRoute,
@@ -59,14 +60,31 @@ export class EventComponent implements OnDestroy{
 
 
   createEvent() {
+    if (!this.event.eventDay) {
+      this.errorMessage = 'Заполните дату события';
+      return;
+    }
+    if (!this.event.title) {
+      this.errorMessage = 'Название не может быть пустым';
+      return;
+    }
+    if (!this.event.description) {
+      this.errorMessage = 'Описание не может быть пустым';
+      return;
+    }
+    if (!this.event.city) {
+      this.errorMessage = 'Город не может быть пустым';
+      return;
+    }
     this.event.createUser = this.currentUser;
     this.event.eventCreatedDate = new Date();
     // this.event.eventDay = this.event.eventDay;
     this.subs.sink = this.eventController.saveEvent(this.event)
       .pipe(
         map(value => {
+          console.log('asddddd', this.eventPhotos)
           if (!!this.eventPhotos){
-            this.subs.sink = this.eventController.saveMultipart(this.eventPhotos, value).subscribe();
+            this.subs.sink = this.eventController.saveMultipart(this.eventPhotos, value).pipe().subscribe();
           }
           this.router.navigate(['profile']).then();
         })
